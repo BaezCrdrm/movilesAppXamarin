a47cede831058e7a959346ccda663ca47654992f
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-08-26 19:38:49 -0500

    Creation of the android fragments

--- MovilesApp/MovilesApp.Android/MainActivity.cs
+++ MovilesApp/MovilesApp.Android/MainActivity.cs
@@ -12,8 +12,6 @@ namespace MovilesApp.Droid
 	[Activity (Label = "MovilesApp.Android", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
@@ -21,13 +19,7 @@ namespace MovilesApp.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
 			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
 		}
 	}
 }
--- MovilesApp/MovilesApp.Android/Resources/Resource.designer.cs
+++ MovilesApp/MovilesApp.Android/Resources/Resource.designer.cs
@@ -60,8 +60,11 @@ namespace MovilesApp.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int fragmentMain = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int textView1 = 2131034112;
 			
 			static Id()
 			{
@@ -77,7 +80,13 @@ namespace MovilesApp.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int fragment_all_events = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int fragment_event_details = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Main = 2130903042;
 			
 			static Layout()
 			{
